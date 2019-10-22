1def955c6c4c48536375c264ec9eeb3ffa563663
lvxianhe <lvxianhe1@sensetime.com>
2019-10-22 21:22:11 +0800

    添加登录页

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,5 +1,23 @@
 <template>
-  <h1>hello</h1>
+  <div class="background">
+    <nav>
+      <p style="float:left; margin-left: 1rem; font-size: 0.3rem">senseMeow</p>
+      <p style="float:right; margin-right:0.2rem">
+        <router-link :to="{path: '/'}" tag="div">登陆/注册</router-link>
+      </p>
+      <p style="float:right; margin-right:0.2rem">
+        <router-link :to="{path: '/'}" tag="div">控制台</router-link>
+      </p>
+    </nav>
+    <div class="main">
+      <div class="display">
+      </div>
+      <div class="display_upper">
+      </div>
+      <div class="control">
+      </div>
+    </div>
+  </div>
 </template>
 <script>
 export default {
@@ -12,4 +30,85 @@ export default {
 }
 </script>
 <style scoped>
+  body,div,ul,li,h1{margin:0;padding:0;font-size:18px;}
+  .background {
+    position: absolute;
+    left: 0; right: 0; top: 0; bottom: 0;
+    width:100%;
+    /*background: url("../assets/2.jpg");*/
+    background: rgba(255,194,45,0.82);
+    background-size: auto;
+  }
+  nav {
+    height: 10%;
+    width: 100%;
+    line-height: 10%;
+    color: #fff;
+    font-weight:bold;
+  }
+  nav::after {
+    content: '';
+    height: 10%;
+    width: 100%;
+    background-color: red;
+    z-index: 100;
+  }
+  .main {
+    height: 90%;
+    width: 90%;
+    display: flex;
+    flex-wrap: wrap;
+    margin: 0 5%;
+    background: pink;
+    position: absolute;
+  }
+  @media only screen and (max-width: 800px) {
+    .display {
+      width: 90%;
+      height: 50%;
+      background: orange;
+      margin-left: 5%;
+      position: absolute;
+    }
+    .control {
+      width: 90%;
+      height: 10%;
+      background: #e9e9e9;
+      margin-left: 5%;
+      margin-top: -30%;
+      position: relative;
+    }
+    /*透明色*/
+    .display_upper {
+      width: 90%;
+      height: 50%;
+      margin-left: 5%;
+      position: relative;
+      background: hsla(0, 0%, 100%, .5);
+    }
+  }
+  @media only screen and (min-width: 800px) {
+    .display {
+      width: 80%;
+      height: 90%;
+      position: absolute;
+      background: rosybrown;
+      margin-left: 5%;
+    }
+    .control {
+      width: 5%;
+      height: 90%;
+      background: #e9e9e9;
+      margin-left: 5%;
+    }
+    /*透明色*/
+    .display_upper {
+      width: 80%;
+      height: 90%;
+      margin-left: 5%;
+      position: relative;
+      background: hsla(0, 0%, 100%, .5);
+    }
+  }
+
 </style>
--- src/components/login.vue
+++ src/components/login.vue
@@ -1,25 +1,35 @@
 <template>
-  <div class="g-container">
-    <h2>登录</h2>
-    <div class="g-username">
-      <input name="loginPhoneOrEmail" maxlength="64" placeholder="请输入手机号或邮箱" class="input">
-      <img src="https://b-gold-cdn.xitu.io/v3/static/img/greeting.1415c1c.png" class="g-username">
+  <div class="background">
+    <nav>
+      <p style="float:left">
+        <img src="../assets/camera.png">
+      </p>
+    </nav>
+    <div class="main">
+      <div class="cat">
+        <img src="../assets/todayPlay.png" class="todayPlay">
+        <img src="../assets/cat.png" class="catPng">
+      </div>
+      <div class="login">
+        <div class="login_title">SenseMeow</div>
+        <div class="login_body">
+          <p class="login_type">密码登陆</p>
+          <p class="login_type">邀请码登陆</p>
+          <div class="g-username">
+            <input name="loginPhoneOrEmail" maxlength="64" placeholder="手机号/用户名" class="input">
+          </div>
+          <div class="g-password">
+            <input name="loginPassword" type="password" maxlength="64" placeholder="密码" class="input">
+          </div>
+          <button class="button_login">
+            登录
+          </button>
+          <p class="signIn">忘记密码|注册账号</p>
+        </div>
+      </div>
     </div>
-
-    <div class="g-password">
-      <input name="loginPassword" type="password" maxlength="64" placeholder="请输入密码" class="input">
-      <img src="https://b-gold-cdn.xitu.io/v3/static/img/blindfold.58ce423.png" class="g-password">
-    </div>
-
-    <img src="https://b-gold-cdn.xitu.io/v3/static/img/normal.0447fe9.png" class="g-normal">
-
-<!--    <div class="g-password">-->
-<!--      <input name="loginPassword" type="password" maxlength="64" placeholder="请输入密码" class="input">-->
-<!--    </div>-->
-
   </div>
 </template>
-
 <script>
 export default {
   name: 'login',
@@ -30,74 +40,120 @@ export default {
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  .g-container {
-    position: relative;
-    width: 318px;
-    margin: 100px auto;
-    height: 370px;
-    padding: 20px;
-    box-sizing: border-box;
-    background: #fff;
-    z-index: 10;
-  }
-  .g-container h2 {
-    font-size: 20px;
-    font-weight: bold;
-    margin-bottom: 30px;
-  }
-  .g-container input {
-    outline: none;
-    padding: 10px;
-    width: 100%;
-    border: 1px solid #e9e9e9;
-    border-radius: 2px;
-    box-sizing: border-box;
-    font-size: 16px;
-  }
-
-  img {
+  body,div,ul,li,h1{margin:0;padding:0;font-size:18px}
+  .background {
     position: absolute;
-    top: -20%;
-    left: 50%;
-    width: 120px;
-    height: 95px;
-    -webkit-transform: translate(-50%, 0);
-    transform: translate(-50%, 0);
+    left: 0; right: 0; top: 0; bottom: 0;
+    width:100%;
+    background-color: rgba(255,194,45,0.82);
+    background-size: auto;
   }
-
+  nav {
+    height: 15%;
+    width: 100%;
+    line-height: 10%;
+    color: #fff;
+    font-weight:bold;
+  }
+  nav::after {
+    content: '';
+    height: 15%;
+    width: 100%;
+    background-color: red;
+    z-index: 100;
+  }
+  .main {
+    height: 80%;
+    width: 90%;
+    display: flex;
+    flex-wrap: wrap-reverse;
+    margin: 0 5%;
+    position: absolute;
+    /*background: pink;*/
+  }
+  /*手机*/
+  @media only screen and (max-width: 800px) {
+    .login {
+      width: 60%;
+      height: 40%;
+      /*background: white;*/
+      margin-left: 20%;
+      float: right;
+    }
+    .cat {
+      width: 60%;
+      height: 40%;
+      /*background: #e9e9e9;*/
+      margin-left: 20%;
+      float: left;
+    }
+  }
+  @media only screen and (min-width: 800px) {
+    .login {
+      width: 32%;
+      height: 85%;
+      /*background: #e9e9e9;*/
+      margin-bottom: 5%;
+      margin-left: 5%;
+      float: right;
+    }
+    .cat {
+      width: 32%;
+      height: 85%;
+      /*background: #e9e9e9;*/
+      margin-left: 18%;
+      margin-bottom: 5%;
+      float: left;
+    }
+  }
+  .login_title {
+    width: 100%;
+    height: 16%;
+    font-size: 0.43rem;
+    color: #fff;
+    font-weight: bold;
+    margin: auto;
+    margin-top: 1%;
+    text-align: center;
+  }
+  .login_body {
+    width: 90%;
+    height: 66%;
+    color: #351900;
+    margin-top: 4%;
+    background-color: #fff;
+    border: 0px solid red;
+    border-radius: 0.3rem;
+    font-size: 0.16rem;
+    padding: 6%;
+  }
+  .login_type {
+    display: inline;
+    float: left;
+    margin: 4%;
+    /*margin-left: 7%;*/
+  }
+  .input {
+    outline: none;
+    padding: 3%;
+    width: 90%;
+    height: 7%;
+    border: 1px solid #e9e9e9;
+    border-radius: 0.02rem;
+    box-sizing: border-box;
+    font-size: 0.13rem;
+  }
   .g-username {
-    margin-bottom: 10px;
+    margin-top: 0.9%;
+    margin-bottom: 3%;
   }
-  .g-username img {
-    display: none;
-    width: 120px;
-    height: 113px;
-  }
-  /*~ 查找指定元素后的所有兄弟节点*/
-  .g-username:focus-within ~ img {
-    display: none;
-  }
-
   .g-username:focus-within input {
     border-color: #007fff;
   }
-  .g-username:focus-within img {
-    display: block;
-  }
-
   .g-password {
-    margin-bottom: 10px;
+    margin-bottom: 0.2%;
   }
-  .g-password img {
-    display: none;
-    width: 103px;
-    height: 84px;
-    top: -15%;
-  }
-
   .g-password:focus-within ~ img {
     display: none;
   }
@@ -105,7 +161,35 @@ export default {
   .g-password:focus-within input {
     border-color: #007fff;
   }
-  .g-password:focus-within img {
-    display: block;
+  .button_login {
+    width: 40%;
+    height: 15%;
+    background-color: #FF9F00;
+    color: #fff;
+    font-size: 0.15rem;
+    border-radius: 1rem;
+    margin-top: 5%;
   }
+  .signIn {
+    margin-top: 4%;
+    color: #351900;
+    font-size: 0.12rem;
+  }
+  .todayPlay {
+    position: relative;
+    margin: auto;
+    /*left: 10%;*/
+    top: 15%;
+    width: 70%;
+    height: 8%;
+  }
+  .catPng {
+    position: relative;
+    margin: auto;
+    /*left: 10%;*/
+    top: 20%;
+    width: 80%;
+    height: 70%;
+  }
+
 </style>
